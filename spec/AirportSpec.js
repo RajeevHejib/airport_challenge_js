818f72843f9d64d5a6cd16432d17e30ac90921f9
Ivan Sathianathan <ivan.sathianathan@gmail.com>
2015-11-10 17:57:04 +0000

    planes taking off must be already at airport

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -6,10 +6,8 @@ describe("Airport", function() {
   beforeEach(function() {
     airport = new Airport();
     plane = new Plane();
-    // weather = new Weather();
     planes = [];
     CAPACITY = airport.CAPACITY;
-
   });
 
   it("cannot land if airport full", function(){
@@ -25,4 +23,24 @@ describe("Airport", function() {
     expect(function(){airport.land(plane); }).toThrow("cannot land plane, weather is stormy");
   });
 
+  it ("cannot let planes take off if weather is stormy", function(){
+    spyOn(Weather.prototype, "isStormy").and.returnValue(true);
+    expect(function(){ airport.take_off(plane); }).toThrow("cannot take off plane, weather is stormy");
+  });
+
+  it ("cannot let planes take off if plane is not at airport", function(){
+    spyOn(Weather.prototype, "isStormy").and.returnValue(false);
+    expect(function(){ airport.take_off(plane); }).toThrow("plane is not at this airport");
+  });
+
+  it ("lets planes take off when not stormy and plane is at airport", function(){
+    spyOn(Weather.prototype, "isStormy").and.returnValue(false);
+    // airport.land(plane);
+    // a = planes.length;
+    // airport.take_off(plane)
+    // b = planes.length;
+    // expect(b === a - 1).toBe(true);
+    airport.land(plane);
+    expect(airport.take_off(plane)).toBe(plane)
+  });
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,20 +1,30 @@
 function Airport(){
 
 var plane;
-var planes = [];
+this.planes = [];
 this.CAPACITY = 20;
 weather = new Weather();
-
 }
 
 Airport.prototype.land = function(plane){
-
   if (weather.isStormy()) {
     throw "cannot land plane, weather is stormy";
   }
-  if (planes.length < CAPACITY) {
-    planes.push(plane);
+  if (this.planes.length < CAPACITY) {
+    this.planes.push(plane);
   } else {
       throw "cannot land plane, airport is full";
     }
+};
+
+Airport.prototype.take_off = function(plane){
+  if (weather.isStormy()) {
+    throw "cannot take off plane, weather is stormy";
+  }
+  if (this.planes.indexOf(plane) === -1) {
+    throw "plane is not at this airport";
+  } else {
+    this.planes.splice(planes.indexOf(plane),1);
+    return plane;
+  }
 };
